35cd42c306f53652d87a4476cf51f541164e4136
JackieBinya <jacquelinebinya@gmail.com>
2020-06-06 12:53:06 +0200

    Adds more features

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,1 +1,31 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
+
+export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const newStoriesUrl = `${baseUrl}newstories.json`;
+export const storyUrl = `${baseUrl}item/`;
+
+export const getStory = async (storyId) => {
+  try {
+    const {
+      data: {
+        title, time, url, by,
+      },
+    } = await axios.get(`${storyUrl + storyId}.json`);
+
+    return {
+      title, time, url, by,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getStoryIds = async () => {
+  try {
+    const { data } = await axios.get(newStoriesUrl);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};

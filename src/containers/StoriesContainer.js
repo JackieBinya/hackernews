2e0e0481bb4456d422ecafe2a3a17e2d8965d463
JackieBinya <jacquelinebinya@gmail.com>
2020-06-26 11:13:43 +0200

    Adds infinite scroller

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
+import { useInfiniteScroll } from '../hooks/infiniteScroll';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
+  const { count }  = useInfiniteScroll();
+  console.log(count)
   useEffect(() => {
     getStoryIds().then(setStoryIds);
   }, []);
   return (
     <>
       <h1>Hacker News</h1>
-      {storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />)}
-
+      {storyIds && storyIds.slice(0, count).map((storyId) => <Story key={storyId} storyId={storyId} />)}
     </>
   );
 };
